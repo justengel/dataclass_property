d0a118c58bb6027cbd913a5c72ee7d1388f183c9
justengel <jtengel08@gmail.com>
2020-12-26 10:47:52 -0500

    Added Date, Time, DateTime types.

--- dataclass_property/__init__.py
+++ dataclass_property/__init__.py
@@ -4,9 +4,8 @@ from .__meta__ import version as __version__
 from dataclasses import Field, field, MISSING, FrozenInstanceError, InitVar, fields, asdict, astuple, make_dataclass, \
     replace, is_dataclass
 from .internals import dataclass, field_property
-from .datetime_utils import TIME_FORMATS, DATE_FORMATS, DATETIME_FORMATS, \
-    datetime_property, time_property, date_property, timedelta_helper_property, seconds_property, DateTime, \
-    make_time, make_date, make_datetime, str_date, str_time, str_datetime
+from .datetime_utils import Date, Time, DateTime, TimeDelta, \
+    datetime_property, time_property, date_property, timedelta_helper_property, seconds_property
 from .weekdays_list import Weekdays, weekdays_property
 
 
@@ -26,9 +25,8 @@ __all__ = ['dataclass', 'field_property',
            'is_dataclass',
 
            # datetime
-           'TIME_FORMATS', 'DATE_FORMATS', 'DATETIME_FORMATS',
+           'Date', 'Time', 'DateTime', 'TimeDelta',
            'datetime_property', 'time_property', 'date_property', 'timedelta_helper_property', 'seconds_property',
-           'DateTime', 'make_time', 'make_date', 'make_datetime', 'str_date', 'str_time', 'str_datetime',
 
            # Weekdays
            'Weekdays', 'weekdays_property',
--- dataclass_property/datetime_utils.py
+++ dataclass_property/datetime_utils.py
@@ -3,33 +3,12 @@ from typing import Union, List
 from dataclass_property import field_property, MISSING
 
 
-__all__ = ['TIME_FORMATS', 'DATE_FORMATS', 'DATETIME_FORMATS',
-           'datetime_property', 'time_property', 'date_property', 'timedelta_helper_property', 'seconds_property',
-           'DateTime', 'make_time', 'make_date', 'make_datetime', 'str_date', 'str_time', 'str_datetime']
+__all__ = ['Date', 'Time', 'DateTime', 'TimeDelta',
+           'datetime_property', 'time_property', 'date_property', 'timedelta_helper_property', 'seconds_property']
 
 
-TIME_FORMATS = [
-    '%I:%M:%S %p',     # '02:24:55 PM'
-    '%I:%M:%S.%f %p',  # '02:24:55.000200 PM'
-    '%I:%M %p',        # '02:24 PM'
-    '%H:%M:%S',        # '14:24:55'
-    '%H:%M:%S.%f',     # '14:24:55.000200'
-    '%H:%M',           # '14:24'
-    ]
-
-DATE_FORMATS = [
-    '%Y-%m-%d', '%m/%d/%Y',   # '2019-04-17', '04/17/2019'
-    '%b %d %Y', '%b %d, %Y',  # 'Apr 17 2019', 'Apr 17, 2019'
-    '%d %b %Y', '%d %b, %Y',  # '17 Apr 2019', '17 Apr, 2019'
-    '%B %d %Y', '%B %d, %Y',  # 'April 17 2019', 'April 17, 2019'
-    '%d %B %Y', '%d %B, %Y',  # '17 April 2019', '17 April, 2019'
-    ]
-
-DATETIME_FORMATS = [d + ' ' + t for t in TIME_FORMATS for d in DATE_FORMATS] + DATE_FORMATS + TIME_FORMATS
-
-
-def datetime_property(attr, allow_none=True, default=MISSING, default_factory=MISSING, formats: List[str] = None):
-    """Create a datetime dataclass property where the underlying datetime is saved to "_attr".
+def date_property(attr, allow_none=True, default=MISSING, default_factory=MISSING, formats: List[str] = None):
+    """Create a date dataclass property where the underlying datetime is saved to "_attr".
 
     Args:
         attr (str): Attribute name (example: "created_on"
@@ -39,12 +18,12 @@ def datetime_property(attr, allow_none=True, default=MISSING, default_factory=MI
         formats (list)[None]: List of string formats to accept.
 
     Returns:
-        property (field_property): Dataclass field property for a datetime.
+        property (field_property): Dataclass field property for a date.
     """
     attr = '_' + attr
-    typeref = Union[datetime.datetime, str]
+    typeref = Union[Date, str]
     if allow_none:
-        typeref = Union[datetime.datetime, str, None]
+        typeref = Union[Date, str, None]
 
     if default == MISSING and default_factory == MISSING and allow_none:
         default = None
@@ -54,12 +33,12 @@ def datetime_property(attr, allow_none=True, default=MISSING, default_factory=MI
 
     def fset(self, value: typeref):
         if value is None and not allow_none:
-            raise TypeError('Invalid datetime value given!')
+            raise TypeError('Invalid date value given!')
         elif value is not None:
-            value = make_datetime(value, formats=formats)
+            value = Date.make_date(value, formats=formats)
         setattr(self, attr, value)
 
-    return field_property(fget, fset, doc='datetime property {}'.format(attr),
+    return field_property(fget, fset, doc='date property {}'.format(attr),
                           default=default, default_factory=default_factory)
 
 
@@ -77,9 +56,9 @@ def time_property(attr, allow_none=True, default=MISSING, default_factory=MISSIN
         property (field_property): Dataclass field property for a time.
     """
     attr = '_' + attr
-    typeref = Union[datetime.time, str]
+    typeref = Union[Time, str]
     if allow_none:
-        typeref = Union[datetime.time, str, None]
+        typeref = Union[Time, str, None]
 
     if default == MISSING and default_factory == MISSING and allow_none:
         default = None
@@ -91,15 +70,15 @@ def time_property(attr, allow_none=True, default=MISSING, default_factory=MISSIN
         if value is None and not allow_none:
             raise TypeError('Invalid time value given!')
         elif value is not None:
-            value = make_time(value, formats=formats)
+            value = Time.make_time(value, formats=formats)
         setattr(self, attr, value)
 
     return field_property(fget, fset, doc='time property {}'.format(attr),
                           default=default, default_factory=default_factory)
 
 
-def date_property(attr, allow_none=True, default=MISSING, default_factory=MISSING, formats: List[str] = None):
-    """Create a date dataclass property where the underlying datetime is saved to "_attr".
+def datetime_property(attr, allow_none=True, default=MISSING, default_factory=MISSING, formats: List[str] = None):
+    """Create a datetime dataclass property where the underlying datetime is saved to "_attr".
 
     Args:
         attr (str): Attribute name (example: "created_on"
@@ -109,12 +88,12 @@ def date_property(attr, allow_none=True, default=MISSING, default_factory=MISSIN
         formats (list)[None]: List of string formats to accept.
 
     Returns:
-        property (field_property): Dataclass field property for a date.
+        property (field_property): Dataclass field property for a datetime.
     """
     attr = '_' + attr
-    typeref = Union[datetime.time, str]
+    typeref = Union[DateTime, str]
     if allow_none:
-        typeref = Union[datetime.time, str, None]
+        typeref = Union[DateTime, str, None]
 
     if default == MISSING and default_factory == MISSING and allow_none:
         default = None
@@ -124,12 +103,12 @@ def date_property(attr, allow_none=True, default=MISSING, default_factory=MISSIN
 
     def fset(self, value: typeref):
         if value is None and not allow_none:
-            raise TypeError('Invalid date value given!')
+            raise TypeError('Invalid datetime value given!')
         elif value is not None:
-            value = make_date(value, formats=formats)
+            value = DateTime.make_datetime(value, formats=formats)
         setattr(self, attr, value)
 
-    return field_property(fget, fset, doc='date property {}'.format(attr),
+    return field_property(fget, fset, doc='datetime property {}'.format(attr),
                           default=default, default_factory=default_factory)
 
 
@@ -228,8 +207,59 @@ def seconds_property(attr='seconds'):
     return field_property(fget, fset, doc='Seconds in time. If float also set milliseconds', default=0)
 
 
-class DateTime(datetime.datetime):
+class Date(datetime.date):
     """DateTime class for pydantic which converts custom string formats into datetimes."""
+
+    DATE_FORMATS = [
+        '%Y-%m-%d', '%m/%d/%Y',   # '2019-04-17', '04/17/2019'
+        '%b %d %Y', '%b %d, %Y',  # 'Apr 17 2019', 'Apr 17, 2019'
+        '%d %b %Y', '%d %b, %Y',  # '17 Apr 2019', '17 Apr, 2019'
+        '%B %d %Y', '%B %d, %Y',  # 'April 17 2019', 'April 17, 2019'
+        '%d %B %Y', '%d %B, %Y',  # '17 April 2019', '17 April, 2019'
+        ]
+
+    @classmethod
+    def make_date(cls, date_string: Union[datetime.date, str], formats: List[str] = None) -> datetime.date:
+        """Make the date object from the given time string.
+
+        Args:
+            date_string (str): Date string 'mm/dd/yyyy' ...
+            formats (list): List of acceptable time string formats.
+
+        Returns:
+            d (datetime.date): Date object or None.
+        """
+        if isinstance(date_string, datetime.date):
+            return date_string
+
+        if formats is None:
+            formats = [i for i in cls.DATE_FORMATS]
+
+        for fmt in formats:
+            try:
+                dt = datetime.datetime.strptime(date_string, fmt)
+                return dt.date()
+            except (TypeError, ValueError, Exception):
+                pass
+
+        raise ValueError('Invalid date format {}. Allowed formats are {}'.format(repr(date_string), repr(formats)))
+
+    @classmethod
+    def str_date(cls, dt: datetime.date) -> str:
+        """Return the date as a string."""
+        return dt.strftime(cls.DATE_FORMATS[0])
+
+    def __new__(cls, year, month=0, day=0):
+        if isinstance(year, bytes):
+            year = year.decode()
+        if isinstance(year, (str, datetime.time)):
+            dt = cls.make_date(year)
+            return dt
+            # year = dt.year
+            # month = dt.month
+            # day = dt.day
+        super().__new__(year=year, month=month, day=day)
+
     @classmethod
     def __get_validators__(cls):
         # one or more validators may be yielded which will be called in the
@@ -240,100 +270,179 @@ class DateTime(datetime.datetime):
     @classmethod
     def __modify_schema__(cls, field_schema):
         field_schema.update(
-                allowed_formats=DATETIME_FORMATS,
+                allowed_formats=cls.DATE_FORMATS,
                 )
 
     @classmethod
     def validate(cls, v):
         if v is None:
             return None
-        return make_datetime(v)
+        return Date(v)
 
     def __str__(self):
-        return str_datetime(self)
+        return self.str_date(self)
 
 
-def make_time(time_string: Union[datetime.time, str], formats: List[str] = None):
-    """Make the time object from the given time string.
-    Args:
-        time_string (str): Time string '04:00 PM' ...
-        formats (list): List of acceptable time string formats.
-    Returns:
-        t (datetime.time): Time object or None.
-    """
-    if isinstance(time_string, datetime.time):
-        return time_string
+class Time(datetime.time):
+    """DateTime class for pydantic which converts custom string formats into datetimes."""
 
-    if formats is None:
-        formats = DATETIME_FORMATS
+    TIME_FORMATS = [
+        '%I:%M:%S %p',     # '02:24:55 PM'
+        '%I:%M:%S.%f %p',  # '02:24:55.000200 PM'
+        '%I:%M %p',        # '02:24 PM'
+        '%H:%M:%S',        # '14:24:55'
+        '%H:%M:%S.%f',     # '14:24:55.000200'
+        '%H:%M',           # '14:24'
+        ]
 
-    for fmt in formats:
-        try:
-            dt = datetime.datetime.strptime(time_string, fmt)
-            return dt.time()
-        except (TypeError, ValueError, Exception):
+    @classmethod
+    def make_time(cls, time_string: Union[datetime.time, str], formats: List[str] = None) -> datetime.time:
+        """Make the time object from the given time string.
+
+        Args:
+            time_string (str): Time string '04:00 PM' ...
+            formats (list): List of acceptable time string formats.
+
+        Returns:
+            t (datetime.time): Time object or None.
+        """
+        if isinstance(time_string, datetime.time):
+            return time_string
+
+        if formats is None:
+            formats = [i for i in cls.TIME_FORMATS]
+
+        for fmt in formats:
+            try:
+                dt = datetime.datetime.strptime(time_string, fmt)
+                return dt.time()
+            except (TypeError, ValueError, Exception):
+                pass
+
+        raise ValueError('Invalid time format {}. Allowed formats are {}'.format(repr(time_string), repr(formats)))
+
+    @classmethod
+    def str_time(cls, dt: datetime.time) -> str:
+        """Return the time as a string"""
+        return dt.strftime(cls.TIME_FORMATS[0])
+
+    def __new__(cls, hour=0, minute=0, second=0, microsecond=0, tzinfo=None, *, fold=0):
+        if isinstance(hour, bytes):
+            hour = hour.decode()
+        if isinstance(hour, (str, datetime.time)):
+            dt = cls.make_time(hour)
+            return dt
+            # hour = dt.hour
+            # minute = dt.minute
+            # second = dt.second
+            # microsecond = dt.microsecond
+            # tzinfo = dt.tzinfo
+            # fold = dt.fold
+        super().__new__(cls, hour=hour, minute=minute, second=second, microsecond=microsecond, tzinfo=tzinfo, fold=fold)
+
+    @classmethod
+    def __get_validators__(cls):
+        # one or more validators may be yielded which will be called in the
+        # order to validate the input, each validator will receive as an input
+        # the value returned from the previous validator
+        yield cls.validate
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(
+                allowed_formats=cls.TIME_FORMATS,
+                )
+
+    @classmethod
+    def validate(cls, v):
+        if v is None:
+            return None
+        return Time(v)
+
+    def __str__(self):
+        return self.str_time(self)
+
+
+class DateTime(datetime.datetime):
+    """DateTime class for pydantic which converts custom string formats into datetimes."""
+
+    DATETIME_FORMATS = [d + ' ' + t for t in Time.TIME_FORMATS for d in Date.DATE_FORMATS]
+    DATETIME_FORMATS.extend(Date.DATE_FORMATS)
+    DATETIME_FORMATS.extend(Time.TIME_FORMATS)
+
+    @classmethod
+    def make_datetime(cls, date_string: Union[datetime.datetime, str], formats: List[str] = None) -> datetime.datetime:
+        """Make the datetime from the given date time string.
+
+        Args:
+            date_string (str): Datetime string '04:00 PM' ...
+            formats (list): List of acceptable datetime string formats.
+
+        Returns:
+            dt (datetime.datetime): Datetime object or None.
+        """
+        if isinstance(date_string, datetime.datetime):
+            return date_string
+
+        if formats is None:
+            formats = [i for i in cls.DATETIME_FORMATS]
+
+        for fmt in formats:
+            try:
+                return datetime.datetime.strptime(date_string, fmt)
+            except (TypeError, ValueError, Exception):
+                pass
+
+        try:  # Try ISO format
+            return datetime.datetime.fromisoformat(date_string)
+        except:
             pass
 
-    raise ValueError('Invalid time format {}. Allowed formats are {}'.format(repr(time_string), repr(formats)))
+        raise ValueError('Invalid datetime format {}. Allowed formats are {}'.format(repr(date_string), repr(formats)))
+
+    @classmethod
+    def str_datetime(cls, dt: datetime.datetime) -> str:
+        """Return the datetime as a string."""
+        return dt.strftime(cls.DATETIME_FORMATS[0])
+
+    def __new__(cls, year, month=0, day=0, hour=0, minute=0, second=0, microsecond=0, tzinfo=None, *, fold=0):
+        if isinstance(year, (str, datetime.datetime)):
+            dt = cls.make_datetime(year)
+            return dt
+            # year = dt.year
+            # month = dt.month
+            # day = dt.day
+            # hour = dt.hour
+            # minute = dt.minute
+            # second = dt.second
+            # microsecond = dt.microsecond
+            # tzinfo = dt.tzinfo
+            # fold = dt.fold
+        return super().__new__(cls, year, month=month, day=day, hour=hour, minute=minute, second=second,
+                               microsecond=microsecond, tzinfo=tzinfo, fold=fold)
+
+    @classmethod
+    def __get_validators__(cls):
+        # one or more validators may be yielded which will be called in the
+        # order to validate the input, each validator will receive as an input
+        # the value returned from the previous validator
+        yield cls.validate
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(
+                allowed_formats=cls.DATETIME_FORMATS,
+                )
+
+    @classmethod
+    def validate(cls, v):
+        if v is None:
+            return None
+        return DateTime(v)
+
+    def __str__(self):
+        return self.str_datetime(self)
 
 
-def make_date(date_string: Union[datetime.date, str], formats: List[str] = None):
-    """Make the date object from the given time string.
-    Args:
-        date_string (str): Date string 'mm/dd/yyyy' ...
-        formats (list): List of acceptable time string formats.
-    Returns:
-        d (datetime.date): Date object or None.
-    """
-    if isinstance(date_string, datetime.date):
-        return date_string
-
-    if formats is None:
-        formats = DATETIME_FORMATS
-
-    for fmt in formats:
-        try:
-            dt = datetime.datetime.strptime(date_string, fmt)
-            return dt.date()
-        except (TypeError, ValueError, Exception):
-            pass
-
-    raise ValueError('Invalid date format {}. Allowed formats are {}'.format(repr(date_string), repr(formats)))
-
-
-def make_datetime(date_string: Union[datetime.datetime, str], formats: List[str] = None) -> datetime.datetime:
-    """Make the datetime from the given date time string.
-    Args:
-        date_string (str): Datetime string '04:00 PM' ...
-        formats (list): List of acceptable datetime string formats.
-    Returns:
-        dt (datetime.datetime): Datetime object or None.
-    """
-    if isinstance(date_string, datetime.datetime):
-        return date_string
-
-    if formats is None:
-        formats = DATETIME_FORMATS
-
-    for fmt in formats:
-        try:
-            return DateTime.strptime(date_string, fmt)
-        except (TypeError, ValueError, Exception):
-            pass
-
-    raise ValueError('Invalid datetime format {}. Allowed formats are {}'.format(repr(date_string), repr(formats)))
-
-
-def str_date(dt: datetime.date) -> str:
-    """Return the date as a string."""
-    return dt.strftime(DATE_FORMATS[0])
-
-
-def str_time(dt: datetime.time) -> str:
-    """Return the time as a string"""
-    return dt.strftime(TIME_FORMATS[0])
-
-
-def str_datetime(dt: datetime.datetime) -> str:
-    """Return the datetime as a string."""
-    return dt.strftime(DATETIME_FORMATS[0])
+class TimeDelta(datetime.timedelta):
+    pass
