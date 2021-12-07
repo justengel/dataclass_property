01ea099dd79427742e784e0c33270dc9154512b8
justengel <jtengel08@gmail.com>
2021-12-07 06:36:56 -0500

    Added a regular property to the read-only check. Not really sure if we should automatically make the property a field. Maybe we should just make the field_property a field. For now, I will leave it as changing it would cause unexpected behavior for users.

--- tests/test_read_only.py
+++ tests/test_read_only.py
@@ -8,6 +8,10 @@ def test_read_only():
         def what(self) -> int:
             return 5
 
+        @property
+        def what_is(self) -> bool:
+            return False
+
         # @what.setter
         # def what(self, val):
         #     print('here', val)
@@ -15,12 +19,19 @@ def test_read_only():
     try:
         h = Hello()
         assert h.what == 5
+        assert h.what_is is False
     except AttributeError:
         raise AssertionError('init argument failed!')
 
     try:
         h.what = 7
         raise AssertionError('Invalid test! "what" field_property has a setter!')
+    except AttributeError:
+        pass  # Should hit here
+
+    try:
+        h.what_is = True
+        raise AssertionError('Invalid test! "what_is" property has a setter!')
     except AttributeError:
         pass  # Should hit here
 
